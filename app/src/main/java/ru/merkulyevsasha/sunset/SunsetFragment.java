4aa93986faef2186812674e5a48ad5421d98ed6e
sasha_merkulev <a_lexandr@mail.ru>
2017-02-07 14:27:53 +0300

    Initial commit

---
+++ app/src/main/java/ru/merkulyevsasha/sunset/SunsetFragment.java
@@ -0,0 +1,49 @@
+package ru.merkulyevsasha.sunset;
+
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SunsetFragment extends Fragment {
+
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
+    public static SunsetFragment newInstance(){
+        return new SunsetFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+
+        mSceneView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+
+        return view;
+    }
+
+    private void startAnimation(){
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(mSunView, "y", sunYStart, sunYEnd)
+                .setDuration(3000);
+
+        heightAnimator.start();
+    }
+
+}
